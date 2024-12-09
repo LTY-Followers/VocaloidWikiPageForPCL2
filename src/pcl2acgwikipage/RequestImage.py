26b4d0d49aab70721767d2093a8a7e1a928514d7
薄奚梦灵 <188181427+shimoranla@users.noreply.github.com>
2024-12-09 22:09:25 +0800

    publish

--- src/pcl2acgwikipage/RequestImage.py
+++ src/pcl2acgwikipage/RequestImage.py
@@ -1,12 +1,34 @@
 from urllib import request,parse
 from bs4 import BeautifulSoup
 from PIL import Image
+from http.client import HTTPResponse
+from http.client import HTTPException
+import time
 
 search_api = "https://search.bilibili.com/all"
 
 url_query = {}
 
-def ReadImage(bvid):
+headers = {
+    "User-Agent":"",
+    "referer":"https://www.bilibili.com/",
+    "Host":"search.bilibili.com"
+}
+
+MaxRetry = 5
+
+async def ReadImage(bvid):
     url_query["keywords"] = bvid
+    while not MaxRetry == 0:
+        try:
+            req = request.Request(url=f"{search_api}?{parse.urlencode(url_query)}",headers=headers)
+            resp:HTTPResponse = request.urlopen(req)
+        except HTTPException as e:
+            time.sleep(5*60)
+            continue
+        resp_html = resp.read().decode("utf-8")
+        soup = BeautifulSoup(resp_html,"html.parser")
+        resolve = soup.find_all("img",loading="lazy")
+        resolve.get("src")
 def CoverImage(file_name):
     image.open()
