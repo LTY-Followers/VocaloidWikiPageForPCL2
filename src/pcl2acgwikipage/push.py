f237aecda5c7a5393b4f5989d6f6754c11670697
薄奚梦灵 <188181427+shimoranla@users.noreply.github.com>
2024-12-12 13:05:36 +0800

    maybe

--- src/pcl2acgwikipage/push.py
+++ src/pcl2acgwikipage/push.py
@@ -1,5 +1,23 @@
 from requests import Session
+from time import sleep
 
-def UploadtoMasterAPI(uri,session:Session):
+uri = "https://files.mirror.luolingxue.us.kg/api"
+def UploadAPI(files:dict,session:Session):
+    data = {
+        "files":[]
+    }
+
+    for file,path in files.items():
+        with open(path,"rb") as f:
+            data["files"][file] = f 
+        
     
-    session.put()
+    resp = session.post(url=uri+"/file/stream_upload",data=data)
+    
+    if resp.status_code == 202:
+        resp_json = resp.json()
+        task_id = resp_json["id"]
+        while True:
+            sleep(10)
+            resp = session.get(url=uri+f"/file/stream_upload/status?pid={task_id}")
+            
